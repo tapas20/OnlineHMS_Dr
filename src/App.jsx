28e98c470a60f0f3e4b6e6cb52df2424b0645214
Tapas Jyoti <tapasjyoti8327@gmail.com>
2025-03-31 21:21:05 +0530

    Routing Setup Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Landing from "./Pages/Landing";
+import AboutUs from "./Pages/AboutUs";
+import DoctorDash from "./Pages/DoctorDash";
+import ContactUs from "./Pages/ContactUs";
+import Login from "./Pages/Login"
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Landing />} />
+            <Route path="about" element={<AboutUs />} />
+            <Route path="doctordash" element={<DoctorDash />} />
+            <Route path="contact" element={<ContactUs />} />
+            <Route path="login" element={<Login/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Pages/DoctorDash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DoctorDash = () => {
+  return (
+    <div>DoctorDash</div>
+  )
+}
+
+export default DoctorDash
